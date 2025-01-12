9a4341fcc2accf4331cb70ee6c4185015bac8bf6
Danilo Osorio <odanilo38@gmail.com>
2025-01-12 14:25:50 -0500

    Initial Commit

---
+++ gitIos/gitIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitIosApp.swift
+//  gitIos
+//
+//  Created by Danilo Osorio on 12/01/25.
+//
+
+import SwiftUI
+
+@main
+struct gitIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
